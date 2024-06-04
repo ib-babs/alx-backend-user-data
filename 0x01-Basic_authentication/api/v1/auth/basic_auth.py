c8b3a1441e6134070c52ac69f39f6b4056109f37
ib-babs <babatundeibrahim426@gmail.com>
2024-06-04 22:02:29 +0100

    Adding base64 Decoding method to basic_auth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,7 @@ BasicAuth class
 from typing import List, TypeVar
 from flask import request
 from .auth import Auth
-from base64 import b64encode
+from base64 import b64encode, b64decode
 
 
 class BasicAuth(Auth):
@@ -20,3 +20,14 @@ class BasicAuth(Auth):
                 not authorization_header.startswith('Basic ')):
             return None
         return authorization_header.replace('Basic ', '', 1)
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        '''Decoding base64 enconding characters'''
+        if (not base64_authorization_header or
+                type(base64_authorization_header) != str):
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except:
+            return None
