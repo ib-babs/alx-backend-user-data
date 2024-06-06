731346d3c026168ab53c9bcb6a782f1d8923bec6
ib-babs <babatundeibrahim426@gmail.com>
2024-06-06 22:30:12 +0100

    Typechecking Task 10 added methods

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''Prevents all Session IDs from lost if application stops.'''
 
+from uuid import uuid4
 from models.user_session import UserSession
 from .session_exp_auth import SessionExpAuth
 
@@ -13,16 +14,22 @@ class SessionDBAuth(SessionExpAuth):
             returns the Session ID'''
         if not user_id or type(user_id) != str:
             return None
-        usersession = UserSession(user_id=user_id)
+        usersession = UserSession(user_id=user_id,
+                                  session_id=str(uuid4()))
         usersession.save()
-        return super().create_session(user_id)
+        return usersession.session_id
 
     def user_id_for_session_id(self, session_id=None):
         '''Returns the User ID by requesting UserSession in the\
             database based on session_id'''
         if not session_id or type(session_id) != str:
             return None
-        user_session = UserSession(session_id=session_id)
-        if session_id in user_session.session_id:
-            return user_session.user_id
-        return None
+        user_session_obj = UserSession.search()
+        new_session_obj = None
+        if not user_session_obj:
+            return None
+        for user in user_session_obj:
+            if session_id == user.session_id:
+                new_session_obj = user
+                break
+        return new_session_obj.user_id if new_session_obj else None
